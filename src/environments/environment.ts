c32d5eee9c5a312ddf3fbb1391adc7127706405e
Gabriel Gomes <gabrielsousagomes@hotmail.com>
2021-10-14 16:48:48 -0300

    :rocket: - finalizando detalhes do projeto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  jsonServerAPI: 'https://b835-189-110-255-153.ngrok.io/',
+  jsonServerAPI: 'http://localhost:3000/',
   production: false
 };
 
